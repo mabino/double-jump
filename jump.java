d4f20e97b6261b7004f4d98a7f039f3367cb8a87
mabino <mabino@gmail.com>
2023-01-28 11:55:30 -0500

    Create jump.java

---
+++ jump.java
@@ -0,0 +1,22 @@
+package jump;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("jump")
+public class Jump
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Jump() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        MinecraftForge.EVENT_BUS.register(new JumpEventHandler());
+    }
+}
